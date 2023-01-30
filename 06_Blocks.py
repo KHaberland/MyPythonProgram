0f83f19b2ca3ed298f9a7b9402e281fec51506e0
KHaberland <123479876+KHaberland@users.noreply.github.com>
2023-01-30 21:46:59 +0200

    Add files via upload

---
+++ 06_Blocks.py
@@ -0,0 +1,25 @@
+
+
+
+
+print("Hello World!")
+print("")
+
+print("")
+
+print("")
+#Простой цикл while
+i = 7
+while i<10:
+    print(i, end=' ')
+    i=i+1
+
+print("")
+print("")
+# Условный оператор
+a=1
+b=5
+if a == b:
+    print("a равно b")
+else: 
+    print("a не равно b")
